18aaf2afca8fe0594ddab3f8bfaf111f6d79be57
alturist-su <aimsushmitasingh@gmail.com>
2024-05-16 21:42:33 +0530

    Initial commit

---
+++ myApplication/src/test/java/com/springlearn/myApplication/MyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlearn.myApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

bed95491f63251bc3f0525cce305e5e23399f083
Sushmita Singh <83809957+alturist-su@users.noreply.github.com>
2024-05-24 20:03:29 +0530

    Update ResController.java

--- myApplication/src/main/java/com/springlearn/myApplication/restcontroller/ResController.java
+++ myApplication/src/main/java/com/springlearn/myApplication/restcontroller/ResController.java
@@ -6,6 +6,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ResController {
 
+    //inject properties for coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    //Expose endpoint for team info
+    @GetMapping("/teamInfo")
+    public String teamInfo(){
+        return "Coach: "+ coachName + "Team Info: "+teamName;
+    }
+    
+
     @GetMapping("/get")
     public String getName(){
         return "Sushmita Singh";

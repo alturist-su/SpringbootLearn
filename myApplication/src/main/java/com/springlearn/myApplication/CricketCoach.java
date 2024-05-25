2c036eb961775c970bcf6e3667628c1b28adc35e
Sushmita Singh <83809957+alturist-su@users.noreply.github.com>
2024-05-25 21:54:01 +0530

    Update CricketCoach.java

--- myApplication/src/main/java/com/springlearn/myApplication/CricketCoach.java
+++ myApplication/src/main/java/com/springlearn/myApplication/CricketCoach.java
@@ -1,5 +1,7 @@
 package com.springlearn.myApplication;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CricketCoach implements Coach{
     @Override
     public String getDailyUpdate() {

663292432bf59c7bdf2f34d083c9a82e3ab323d4
Sushmita Singh <83809957+alturist-su@users.noreply.github.com>
2024-05-28 01:02:04 +0530

    Update CricketCoach.java

--- myApplication/src/main/java/com/springlearn/myApplication/coach/CricketCoach.java
+++ myApplication/src/main/java/com/springlearn/myApplication/coach/CricketCoach.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CricketCoach implements Coach{
+
+    public CricketCoach(){
+        System.out.println("In constructor: "+ getClass().getSimpleName());
+    }
+    
     @Override
     public String getDailyUpdate() {
         return "Practice fast bowling";

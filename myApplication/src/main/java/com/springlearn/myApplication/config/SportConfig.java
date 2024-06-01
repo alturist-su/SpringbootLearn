4762187b1bc7db8b45ab522afb3a8e93bddd52dd
Sushmita Singh <83809957+alturist-su@users.noreply.github.com>
2024-06-01 23:58:12 +0530

    Update SportConfig.java

--- myApplication/src/main/java/com/springlearn/myApplication/config/SportConfig.java
+++ myApplication/src/main/java/com/springlearn/myApplication/config/SportConfig.java
@@ -1,1 +1,13 @@
+port org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class SportConfig {
+
+    //the bean id defaults to the method name
+    //for custom bean id:
+    @Bean("aquatic")
+    public Coach swimCoach(){
+        return new SwimCoach();
+    }
+}

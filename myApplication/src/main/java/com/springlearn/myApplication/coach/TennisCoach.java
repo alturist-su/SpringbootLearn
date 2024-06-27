50ad73dceaf307524c917104332d5ea1d324c258
Sushmita Singh <83809957+alturist-su@users.noreply.github.com>
2024-06-27 21:53:39 +0530

    Update TennisCoach.java

--- myApplication/src/main/java/com/springlearn/myApplication/coach/TennisCoach.java
+++ myApplication/src/main/java/com/springlearn/myApplication/coach/TennisCoach.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach{
 
+    //tennis Coach 
     public TennisCoach(){
         System.out.println("In constructor: "+ getClass().getSimpleName());
     }
